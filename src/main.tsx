9780c1abfc570e75f4f4f559cdd634ba286ee8f8
Franco Zariaga <francozariaga.zariaga@gmail.com>
2025-08-30 16:16:36 -0300

    refactor(data): add @tanstack/react-query-devtools to test
    Also we added some time in the staleTime to see the state "fresh"
    
    Co-authored-by: LucaTvl <LucaTvl@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App.tsx';
 import './index.css';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -13,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
