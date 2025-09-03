3c480c92911ac6d952eb62d68ef2b497042d4884
Nicolás Pedemonte <pedemontenicolas2004@gmail.com>
2025-09-02 21:15:36 -0300

    chore(project): standardize UI folder structure (#91)

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet, useLocation } from 'react-router-dom';
-import { NavBar } from './NavBar';
+import { NavBar } from '../common/NavBar';
 import { Footer } from '../common/Footer.tsx';
 
 const MainLayout = () => {
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,7 +2,7 @@ import { PopularRanking } from '../components/landing/PopularRanking';
 import { BenefitsSection } from '../components/landing/BenefitsSection';
 import { TrendingCourses } from '../components/landing/TrendingCourses';
 import HeroSection from '../components/landing/HeroSection';
-import { NavBar } from '../components/layouts/NavBar';
+import { NavBar } from '../components/common/NavBar';
 
 export default function LandingPage() {
   return (
