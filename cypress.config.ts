c6dee93261d6e2af19ddad68d8140900b9d8d756
carlex <ricardogugliermino@gmail.com>
2025-12-18 00:28:38 -0300

    ci auto-test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "di6an5", 
+
   e2e: {
     setupNodeEvents() {
       // implement node event listeners here
     },
   },
-});
+})
